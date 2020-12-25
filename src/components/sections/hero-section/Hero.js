77de08de6e1bd783d0669890f1444ccfd9f8dcbb
ezekiel <ezekielnizamani0@gmail.com>
2020-12-25 22:30:48 +0500

    created scial media comeponet and change opacity  of hero overlay 0.3 to 0.5

--- src/components/sections/hero-section/Hero.js
+++ src/components/sections/hero-section/Hero.js
@@ -1,11 +1,10 @@
 
 
 import './Hero.css'
-
+import Icons from '../../social-media-icon/Icons'
 
 const HeroSection = () => (
   <div className="hero">
-    
     {/* hero content */}
     <div className="hero__content">
       <h1 className="heading-1">H! I am Ezekiel</h1>
@@ -14,6 +13,7 @@ const HeroSection = () => (
       <p className="hero-text">
         This website is Designed in adobe xd and developed in react js
       </p>
+  <Icons/>
     </div>
 
     <div className="hero-overlay"></div>
---
+++ src/components/social-media-icon/Icons.js
@@ -0,0 +1,30 @@
+
+import './Icons.css'
+
+const Icons = () => (
+  <>
+    <ul className="icons">
+      <li className="icon">
+        <a href="https://www.linkedin.com/in/ezekiel-nizamani-51a3661a6/">
+          <i class="fab fa-linkedin-in"></i>
+        </a>
+      </li>
+      <li className="icon">
+        <a href=" https://github.com/ezekielnizamani">
+          <i class="fab fa-github"></i>
+        </a>
+      </li>
+      <li className="icon">
+        <a href="https://www.facebook.com/hazqeel.niz.2/">
+          <i class="fab fa-facebook-f"></i>
+        </a>
+      </li>
+      <li className="icon">
+        <a href="https://www.instagram.com/ezekiel.dev26/">
+          <i class="fab fa-instagram"></i>
+        </a>
+      </li>
+    </ul>
+  </>
+);
+export default Icons;
